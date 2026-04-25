decd2f940650e1dc80fde527c8fbcad977824597
ThiagoDanielPerez <thiagodanielperez@alu.frlp.utn.edu.ar>
2026-04-25 20:30:31 -0300

    feat(backend): desarrolla endpoint /api/team con conexión a base de datos

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,6 @@
+import os
+import psycopg2
+from psycopg2.extras import RealDictCursor #las consultas a la base de datos se devuelven como diccionarios.
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -8,6 +11,36 @@ def health_check():
         "status": "activo", 
         "mensaje": "El backend está funcionando correctamente"
     }), 200
+
+@app.route('/api/team', methods=['GET'])
+def get_team():
+    try:
+        host = os.environ.get('DB_HOST', 'database') 
+        port = os.environ.get('DB_PORT', '5432')
+        user = os.environ.get('DB_USER', 'postgres')
+        password = os.environ.get('DB_PASSWORD', 'postgres')
+        dbname = os.environ.get('DB_NAME', 'teamboard_db')
+
+        conn = psycopg2.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            dbname=dbname
+        )
+        
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
+        cursor.execute("SELECT * FROM members;")
+        members = cursor.fetchall()
+
+        cursor.close()
+        conn.close()
+
+        return jsonify(members), 200
+
+    except Exception as e:
+        return jsonify({"error": "Error al conectar con la base de datos", "detalle": str(e)}), 500
+
 
 @app.route('/api/info', methods=['GET'])
 def info():
